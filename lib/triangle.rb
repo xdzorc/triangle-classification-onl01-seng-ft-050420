600d4e1f2b52f7aab4c8aa24d3ad4001c8ae4bad
DAZUN XING <dazun.xing@gmail.com>
2020-05-24 00:44:57 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c, :array
+  # @array =[]
+  def initialize(a,b,c)
+    @a =a
+    @b =b
+    @c =c
+    @array =[@a,@b,@c]
+  end
+  
+  def kind 
+    # array =[@a,@b,@c]
+    if @array.all? {|x| x>0 && (@array.sum -x)>x}
+      if @a==@b && @b==@c 
+        return :equilateral
+      elsif (@array.length) -(@array.uniq.length) !=0
+        return :isosceles
+      else
+        return :scalene
+      end
+  
+     else
+         raise TriangleError
+    end
+  end
+  
+  class TriangleError < StandardError
+  end
+    
 end
